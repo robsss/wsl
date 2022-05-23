21026db24fec46af3ba3887db59c49ac24e5c6f3
robbss <y1573908972@gmail.com>
2022-05-23 11:10:54 +0800

    Rewrite the installation shell script

--- wsl_config.sh
+++ wsl_config.sh
@@ -1,5 +1,28 @@
 #!bin/bash
 # this shell script will install zsh
 
-sudo apt install zsh git 
+sudo apt install zsh git vim neovim tilix tmux 
+
+# proxychains proxychains-ng
+sudo apt install  proxychains proxychains-ng 
+
+# newsboat
+sudo apt install newsboat
+
+# oh-my-zsh
+
+# conda
+proxychains wget https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh
+bash Anaconda3-2022.05-Linux-x86_64.sh
+
+# vscode
+code
+
+# chrome
+cd /tmp
+sudo proxychains wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# ranger
+sudo apt install ranger
 
